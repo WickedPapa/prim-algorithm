3f6cecb1f4a522ec3d14254de75191404e647b9f
skymontanop <pamontan@emeal.nttdata.com>
2026-01-21 17:31:38 +0100

    feat: first commit

---
+++ src/main/java/com/exercise/prim_algorithm/PrimAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.prim_algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimAlgorithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/prim_algorithm/PrimAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.prim_algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
